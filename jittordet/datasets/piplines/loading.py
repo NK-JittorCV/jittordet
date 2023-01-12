4c31bf12d5949cce43e56fab7fd259a0db88f660
cym <chenyuming@nankai.edu.cn>
2023-01-12 21:53:49 +0800

    [Add] LoadImageFromFile and LoadAnnotations

--- jittordet/datasets/piplines/loading.py
+++ jittordet/datasets/piplines/loading.py
@@ -2,10 +2,11 @@ import numpy as np
 import os.path as osp
 from PIL import Image
 
-# @PIPELINES.register_module()
+from jittordet.engine import TRANSFORM
+
+@TRANSFORM.register_module()
 class LoadImageFromFile:
-    """Load an image from file.
-    """
+    """Load an image from file."""
     def __init__(self,
                  to_float32=False,
                  image_colors=256,
@@ -14,50 +15,38 @@ class LoadImageFromFile:
         self.image_colors = image_colors
         self.image_mode = mode
 
-    def __call__(self, results):
-        """Call functions to load image and get image meta information.
-
-        Args:
-            results (dict): Result dict from :obj:`mmdet.CustomDataset`.
-
-        Returns:
-            dict: The dict contains loaded image and meta information.
-        """
-
-        if results['img_prefix'] is not None:
-            filename = osp.join(results['img_prefix'],
-                                results['img_info']['filename'])
+    def __call__(self, data: dict) -> dict:
+        """Call functions to load image and get image meta information."""
+        if data['img_prefix'] is not None:
+            filename = osp.join(data['img_prefix'],
+                                data['img_info']['filename'])
         else:
-            filename = results['img_info']['filename']
+            filename = data['img_info']['filename']
 
-        img = Image.open(filename).convert(mode=self.image_mode, colors=self.image_colors)
-        img = np.array(img)
+        img = np.array(Image.open(filename).convert(mode=self.image_mode, colors=self.image_colors))
         
         if self.to_float32:
             img = img.astype(np.float32)
 
-        results['filename'] = filename
-        results['ori_filename'] = results['img_info']['filename']
-        results['img'] = img
-        results['img_shape'] = img.shape
-        results['ori_shape'] = img.shape
-        results['img_fields'] = ['img']
-        return results
+        data['filename'] = filename
+        data['ori_filename'] = data['img_info']['filename']
+        data['img'] = img
+        data['img_shape'] = img.shape
+        data['ori_shape'] = img.shape
+        data['img_fields'] = ['img']
+        return data
 
     def __repr__(self):
         repr_str = (f'{self.__class__.__name__}('
                     f'to_float32={self.to_float32}, '
-                    f"color_type='{self.image_colors}', "
-                    f"channel_order='{self.image_mode}', "
-                    f'file_client_args={self.file_client_args})')
+                    f"image_colors='{self.image_colors}', "
+                    f'image_mode={self.image_mode})')
         return repr_str
 
     
-# @PIPELINES.register_module()
+@TRANSFORM.register_module()
 class LoadAnnotations:
-    """Load multiple types of annotations.
-    """
-
+    """Load multiple types of annotations."""
     def __init__(self,
                  with_bbox=True,
                  with_label=True,
@@ -66,61 +55,48 @@ class LoadAnnotations:
         self.with_label = with_label
         self.denorm_bbox = denorm_bbox
 
-    def _load_bboxes(self, results):
-        """Private function to load bounding box annotations.
-        """
-
-        ann_info = results['ann_info']
-        results['gt_bboxes'] = ann_info['bboxes'].copy()
+    def _load_bboxes(self, data: dict) -> dict:
+        """Private function to load bounding box annotations."""
+        ann_info = data['ann_info']
+        data['gt_bboxes'] = ann_info['bboxes'].copy()
 
         if self.denorm_bbox:
-            bbox_num = results['gt_bboxes'].shape[0]
+            bbox_num = data['gt_bboxes'].shape[0]
             if bbox_num != 0:
-                h, w = results['img_shape'][:2]
-                results['gt_bboxes'][:, 0::2] *= w
-                results['gt_bboxes'][:, 1::2] *= h
+                h, w = data['img_shape'][:2]
+                data['gt_bboxes'][:, 0::2] *= w
+                data['gt_bboxes'][:, 1::2] *= h
 
         gt_bboxes_ignore = ann_info.get('bboxes_ignore', None)
         if gt_bboxes_ignore is not None:
-            results['gt_bboxes_ignore'] = gt_bboxes_ignore.copy()
-            results['bbox_fields'].append('gt_bboxes_ignore')
-        results['bbox_fields'].append('gt_bboxes')
+            data['gt_bboxes_ignore'] = gt_bboxes_ignore.copy()
+            data['bbox_fields'].append('gt_bboxes_ignore')
+        data['bbox_fields'].append('gt_bboxes')
 
         gt_is_group_ofs = ann_info.get('gt_is_group_ofs', None)
         if gt_is_group_ofs is not None:
-            results['gt_is_group_ofs'] = gt_is_group_ofs.copy()
+            data['gt_is_group_ofs'] = gt_is_group_ofs.copy()
 
-        return results
+        return data
 
-    def _load_labels(self, results):
-        """Private function to load label annotations.
-        """
-
-        results['gt_labels'] = results['ann_info']['labels'].copy()
-        return results
+    def _load_labels(self, data: dict) -> dict:
+        """Private function to load label annotations."""
+        data['gt_labels'] = data['ann_info']['labels'].copy()
+        return data
 
 
-    def __call__(self, results):
-        """Call function to load multiple types annotations.
-
-        Args:
-            results (dict): Result dict from :obj:`mmdet.CustomDataset`.
-
-        Returns:
-            dict: The dict contains loaded bounding box, label, mask and
-                semantic segmentation annotations.
-        """
-
+    def __call__(self, data: dict) -> dict:
+        """Call function to load multiple types annotations."""
         if self.with_bbox:
-            results = self._load_bboxes(results)
-            if results is None:
+            data = self._load_bboxes(data)
+            if data is None:
                 return None
         if self.with_label:
-            results = self._load_labels(results)
-        return results
+            data = self._load_labels(data)
+        return data
 
     def __repr__(self):
         repr_str = self.__class__.__name__
         repr_str += f'(with_bbox={self.with_bbox}, '
-        repr_str += f'with_label={self.with_label}, '
+        repr_str += f'with_label={self.with_label})'
         return repr_str
