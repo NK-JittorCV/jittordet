65b1c2697c033fddce27d8eb0b64df6cebe990ed
cym <chenyuming@nankai.edu.cn>
2023-01-12 23:49:18 +0800

    [Add] MultiScaleFlipAug

---
+++ jittordet/datasets/transforms/test_time_aug.py
@@ -0,0 +1,70 @@
+import warnings
+
+from jittordet.engine.register import TRANSFORM
+from .transforms import Compose
+
+
+@TRANSFORM.register_module()
+class MultiScaleFlipAug:
+    """Test-time augmentation with multiple scales and flipping."""
+    def __init__(self,
+                 transforms,
+                 img_scale=None,
+                 scale_factor=None,
+                 flip=False,
+                 flip_direction='horizontal'):
+        self.transforms = Compose(transforms)
+        assert (img_scale is None) ^ (scale_factor is None), (
+            'Must have but only one variable can be set')
+        if img_scale is not None:
+            self.img_scale = img_scale if isinstance(img_scale,
+                                                     list) else [img_scale]
+            self.scale_key = 'scale'
+            for i in self.img_scale:
+                assert isinstance(i, tuple)
+        else:
+            self.img_scale = scale_factor if isinstance(
+                scale_factor, list) else [scale_factor]
+            self.scale_key = 'scale_factor'
+
+        self.flip = flip
+        self.flip_direction = flip_direction if isinstance(
+            flip_direction, list) else [flip_direction]
+        for i in self.flip_direction:
+            assert isinstance(i, str)
+        if not self.flip and self.flip_direction != ['horizontal']:
+            warnings.warn(
+                'flip_direction has no effect when flip is set to False')
+        if (self.flip
+                and not any([t['type'] == 'RandomFlip' for t in transforms])):
+            warnings.warn(
+                'flip has no effect when RandomFlip is not in transforms')
+
+    def __call__(self, data: dict) -> dict:
+        """Call function to apply test time augment transforms on results."""
+        aug_data = []
+        flip_args = [(False, None)]
+        if self.flip:
+            flip_args += [(True, direction)
+                          for direction in self.flip_direction]
+        for scale in self.img_scale:
+            for flip, direction in flip_args:
+                _data = data.copy()
+                _data[self.scale_key] = scale
+                _data['flip'] = flip
+                _data['flip_direction'] = direction
+                data = self.transforms(_data)
+                aug_data.append(data)
+        # list of dict to dict of list
+        aug_data_dict = {key: [] for key in aug_data[0]}
+        for data in aug_data:
+            for key, val in data.items():
+                aug_data_dict[key].append(val)
+        return aug_data_dict
+
+    def __repr__(self):
+        repr_str = self.__class__.__name__
+        repr_str += f'(transforms={self.transforms}, '
+        repr_str += f'img_scale={self.img_scale}, flip={self.flip}, '
+        repr_str += f'flip_direction={self.flip_direction})'
+        return repr_str
