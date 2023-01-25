b1b222dbd1b41020ae1381a4c798817ec9401f8c
ubuntu <chenyuming@mail.nankai.edu.cn>
2023-01-26 02:38:59 +0800

    [Update] add encode_size

--- jittordet/models/task_utils/bbox_coders/base_bbox_coder.py
+++ jittordet/models/task_utils/bbox_coders/base_bbox_coder.py
@@ -11,6 +11,8 @@ class BaseBBoxCoder(metaclass=ABCMeta):
         use_box_type (bool): Whether to warp decoded boxes with the
             box type data structure. Defaults to False.
     """
+    
+    encode_size = 4
 
     @abstractmethod
     def encode(self, bboxes, gt_bboxes):
